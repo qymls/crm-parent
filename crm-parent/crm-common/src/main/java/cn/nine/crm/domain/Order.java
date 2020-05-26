df17f64eafec088f29246756ab7991839de7835d
qymls <123@qq.com>
2020-05-26 14:55:00 +0800

    完成动态路由，菜单管理

--- crm-parent/crm-common/src/main/java/cn/nine/crm/domain/Order.java
+++ crm-parent/crm-common/src/main/java/cn/nine/crm/domain/Order.java
@@ -16,7 +16,7 @@ public class Order extends BaseDomain {
     //订单编号 自动生成
     private String sn;
     //客户对象 显示客户姓名
-    private Customer customer;
+    //private Customer customer;
     //签订时间 前端选择
     private Date signTime;
     //营销人员 前端选择 显示人员姓名
