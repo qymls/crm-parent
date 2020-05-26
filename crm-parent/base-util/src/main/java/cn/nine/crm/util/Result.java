2d197e146bee39b607dc2200d74c4989b04fdb1e
gao <1528981120@qq.com>
2020-05-26 19:05:37 +0800

    No.2业务管理更新

--- crm-parent/base-util/src/main/java/cn/nine/crm/util/Result.java
+++ crm-parent/base-util/src/main/java/cn/nine/crm/util/Result.java
@@ -44,27 +44,4 @@ public class Result {
         return result;
     }
 
-    public Boolean getSuccess() {
-        return success;
-    }
-
-    public void setSuccess(Boolean success) {
-        this.success = success;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public Object getData() {
-        return data;
-    }
-
-    public void setData(Object data) {
-        this.data = data;
-    }
 }
