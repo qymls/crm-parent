37b43dae592caa87f36b5b427a592d7cdf7240c9
gao <1528981120@qq.com>
2020-05-26 16:53:22 +0800

    No.2业务管理更新

--- crm-parent/base-core/src/main/java/cn/nine/crm/dto/IdsDto.java
+++ crm-parent/base-core/src/main/java/cn/nine/crm/dto/IdsDto.java
@@ -8,4 +8,12 @@ import java.util.List;
 public class IdsDto {
 
     private List<Long> ids;
+
+    public List<Long> getIds() {
+        return ids;
+    }
+
+    public void setIds(List<Long> ids) {
+        this.ids = ids;
+    }
 }
--- crm-parent/base-util/src/main/java/cn/nine/crm/util/Result.java
+++ crm-parent/base-util/src/main/java/cn/nine/crm/util/Result.java
@@ -43,4 +43,28 @@ public class Result {
         result.setData(data);
         return result;
     }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
 }
