2ec59c976d0c694c3d8a5719e95b143d33298e62
gao <1528981120@qq.com>
2020-05-27 13:38:44 +0800

    No.3业务管理前端更新2

--- crm-parent/crm-web/src/main/java/cn/nine/crm/web/controller/ContractItemController.java
+++ crm-parent/crm-web/src/main/java/cn/nine/crm/web/controller/ContractItemController.java
@@ -3,6 +3,7 @@ package cn.nine.crm.web.controller;
 import cn.nine.crm.domain.ContractItem;
 import cn.nine.crm.query.ContractItemQuery;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @ResponseBody
 @RequestMapping("/contractitem")
+@CrossOrigin
 public class ContractItemController extends BaseController<ContractItem,Long, ContractItemQuery>{
 
 }
--- crm-parent/crm-web/src/main/java/cn/nine/crm/web/controller/OrderController.java
+++ crm-parent/crm-web/src/main/java/cn/nine/crm/web/controller/OrderController.java
@@ -3,6 +3,7 @@ package cn.nine.crm.web.controller;
 import cn.nine.crm.domain.Order;
 import cn.nine.crm.query.OrderQuery;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @ResponseBody
 @RequestMapping("/order")
+@CrossOrigin
 public class OrderController extends BaseController<Order,Long, OrderQuery>{
 
 }
