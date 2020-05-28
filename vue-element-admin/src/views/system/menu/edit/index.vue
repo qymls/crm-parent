d2a9b98a606c563473f23a712fba09900bdeaab4
qymls <123@qq.com>
2020-05-28 19:32:03 +0800

    更改权限提示

--- vue-element-admin/src/views/system/menu/edit/index.vue
+++ vue-element-admin/src/views/system/menu/edit/index.vue
@@ -347,10 +347,10 @@
               dataType: 'json',
               async: false,/*取消异步加载*/
               success: function (results) {
-                var result = results.data
-                if (result.msg) {/*操作失败，无权限*/
-                  $page.$Message.error(result.msg);
+                if (results.msg) {/*操作失败，无权限*/
+                  $page.$Message.error(results.msg);
                 } else {
+                  var result = results.data
                   newMenu = result;
                   var data = $page.tempAppendData;
                   var newMenuData = $.extend({}, result, {title: result.name});/*需要为展开状态*/
