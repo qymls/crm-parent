a2305ec647910e75d057367d0a07c7bd8ff7d42a
qymls <123@qq.com>
2020-05-30 00:02:20 +0800

    顾客管理

---
+++ crm-parent/crm-mapper/src/main/java/cn/nine/crm/mapper/CustomerMapper.java
@@ -0,0 +1,13 @@
+package cn.nine.crm.mapper;
+
+import cn.nine.crm.domain.Customer;
+import cn.nine.crm.query.CustomerQuery;
+/**
+ * (Customer)表数据库访问层
+ *
+ * @author 申林
+ * @since 2020-05-29 22:15:26
+ */
+public interface CustomerMapper extends BaseMapper<Customer,Long,CustomerQuery>{
+
+}
---
+++ crm-parent/crm-service/src/main/java/cn/nine/crm/service/ICustomerService.java
@@ -0,0 +1,13 @@
+package cn.nine.crm.service;
+
+import cn.nine.crm.domain.Customer;
+import cn.nine.crm.query.CustomerQuery;
+/**
+ * (Customer)表Service层接口
+ *
+ * @author 申林
+ * @since 2020-05-29 22:15:34
+ */
+public interface ICustomerService extends IBaseService<Customer,Long,CustomerQuery>{
+
+}
