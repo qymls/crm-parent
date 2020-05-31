ab7dc00e2e998a7db5f046741c96e969d2c9966a
mengzyi <942503123@qq.com>
2020-05-31 22:43:14 +0800

    提交代码，部门有bug确实改不来了。。。

--- vue-element-admin/src/views/system/permission/index.vue
+++ vue-element-admin/src/views/system/permission/index.vue
@@ -214,7 +214,8 @@
           var parentValueList = this.getAllMenuParent(data.menu.id);
           parentValueList.push(data.menu.id.toString())
           this.$nextTick(() => {/*必须放在这个里面，不然值不会刷新的*/
-            this.menuValue = parentValueList;
+            this
+
           })
         }
       },
